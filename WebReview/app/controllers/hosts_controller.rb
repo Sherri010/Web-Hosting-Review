b65323ecc1b38a1ff776a9a03b4864c082f0cc14
sherri010 <aminshahidi.sh@gmail.com>
2016-10-23 10:41:36 -0700

    adds host controller actions

--- WebReview/app/controllers/hosts_controller.rb
+++ WebReview/app/controllers/hosts_controller.rb
@@ -1,2 +1,22 @@
 class HostsController < ApplicationController
+  def index
+    @hosts = Host.all
+  end
+
+  def show
+    @host = Host.find(params[:id])
+    @plans = @host.plans.find(1)
+  end
+  def new
+  end
+  def create
+    @host = Host.create(host_params)
+    redirect_to root_path
+  end
+
+  private
+  def host_params
+    params.require(:host).permit(:name,:url,:logo,:rating,:pros,:cons)
+  end
+
 end
--- WebReview/config/routes.rb
+++ WebReview/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: 'hosts#index'
 
- get '/hosts/:id', to: 'hosts#show'
  get '/hosts/new', to: 'hosts#new'
+ get '/hosts/:id', to: 'hosts#show' , as: "plans"
 post 'hosts'     , to: 'hosts#create'
 end
